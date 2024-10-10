a8348d4af73030f495d1fae9daee852db88c7676
Frangoublicidad <tattoos2012@hotmail.es>
2024-10-10 12:17:44 -0500

    Proyecto

--- src/main/java/com/Banistmo/automation/userinterfaces/HomePageUI.java
+++ src/main/java/com/Banistmo/automation/userinterfaces/HomePageUI.java
@@ -8,10 +8,10 @@ public class HomePageUI {
             .located(By.id("btn-aceptar-cookies"));
     public static final Target CLICK_BUTTON_APRENDER = Target.the("Boton de Aprender")
             .located(By.linkText("Aprender es fácil"));
-    public static final Target CLICK_BUTTON_LEGALES = Target.the("Boton de Aprender")
-            .located(By.xpath("(//img[@alt='Ícono'])[7]"));
-    public static final Target CLICK_BUTTON_FACT = Target.the("Boton de Aprender")
-            .located(By.xpath("(//img[@alt='Ícono'])[2]"));
-    public static final Target CLICK_BUTTON_DOWNLOAD_PDF = Target.the("Boton de Aprender")
+    public static final Target CLICK_BUTTON_LEGALES = Target.the("Boton de legal")
+            .located(By.xpath("(//a[text()='Descubre más'])[5]"));
+    public static final Target CLICK_BUTTON_FACT = Target.the("Boton de Fatca y CRS")
+            .located(By.xpath("(//a[text()='Conoce más'])[2]"));
+    public static final Target CLICK_BUTTON_DOWNLOAD_PDF = Target.the("Boton de descargar")
             .located(By.xpath("(//img[@alt='Descargar PDF'])[3]"));
 }
